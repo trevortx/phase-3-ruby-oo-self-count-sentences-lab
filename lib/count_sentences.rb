3263f0fa1e1b72035665b804fea4a7cf5e1981a1
Trevor Vardeman <trevortx@gmail.com>
2022-06-25 16:12:08 -0700

    commit tree 087a72f223ae3c0bfa464b3e034e20a8b2a9b6a7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,18 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject{ |s| s.empty? }.count
   end
 end
